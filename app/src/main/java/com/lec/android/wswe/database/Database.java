0c09b211bd795602212f0c2f5f332c0b12bb3f4b
Jay9011 <whdtjq5904@gmail.com>
2020-04-22 17:33:52 +0900

    DB 수정

--- app/src/main/java/com/lec/android/wswe/database/Database.java
+++ app/src/main/java/com/lec/android/wswe/database/Database.java
@@ -1,8 +1,24 @@
 package com.lec.android.wswe.database;
 
+import android.content.Context;
+
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @androidx.room.Database(entities = {Restaurant.class}, version = 1)
 public abstract class Database extends RoomDatabase {
+    private static Database INSTANCE;
+
     public abstract RestDAO restDAO();
+
+    public static Database getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context, Database.class, "wsweDB").build();
+        }
+        return INSTANCE;
+    } // end getInstance
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
 }
--- app/src/main/java/com/lec/android/wswe/database/Restaurant.java
+++ app/src/main/java/com/lec/android/wswe/database/Restaurant.java
@@ -20,13 +20,6 @@ public class Restaurant {
         this.star = star;
     }
 
-//    public Restaurant(@NonNull String rest_name, String telephone, int star, int visited) {
-//        this.rest_name = rest_name;
-//        this.telephone = telephone;
-//        this.star = star;
-//        this.visited = visited;
-//    }
-
     public int getRest_id() {
         return rest_id;
     }
